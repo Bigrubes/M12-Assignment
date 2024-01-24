16606d028865e0100323b851bbe18c9807a34e08
Reuben Ramos <rsramos23@gmail.com>
2024-01-24 15:11:22 -0800

    finished 1-4

--- js/oop-practice.js
+++ js/oop-practice.js
@@ -1,11 +1,38 @@
 // STEP 1
+// class Cat{
+//     constructor(){
+// }
+// }
 
+// const Dog = class {
+//     constructor(){
+// }
+// }
 // STEP 2
-
+// const cat1 = new Cat()
+// const dog1 = new Dog()
 // STEP 3
+// class Animal {
+//     constructor(){
 
+//     }
+//     display(){
+//         console.log("The Animal has been created")
+//     }
+// }
+// const newAnimal = new Animal()
+// newAnimal.display()
 // STEP 4
-
+// class Animal{
+//     constructor(message){
+//         this._message = message
+//     }
+//     display(){
+//         console.log(this._message)
+//     }
+// }
+// const newAnimal = new Animal("The Animal has been created")
+// newAnimal.display()
 // STEP 5
 
 // STEP 6
